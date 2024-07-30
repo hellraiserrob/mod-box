de4a48214d3708cd0d0111c71fb9200659bb4c2c
hellraiserrob <robert.phillips1@maersk.com>
2024-07-30 14:34:33 +0100

    block requests and save to storage

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,218 @@
 <template>
   <div>
     <h1>ModBox</h1>
-    <div class="folders">
+    <div v-if="data.folders" class="folders">
       <div class="folders__list">
-        <h2>Folders / Project</h2>
-        <div 
+        <h2>Folders</h2>
+        <div v-if="!data.folders.length">No folders</div>
+        <div
           v-for="(folder, index) in data.folders"
           class="folders__list__item"
           @click="activeFolder = index"
-          :class="{'folders__list__item--active': index === activeFolder}"
+          :class="{ 'folders__list__item--active': index === activeFolder }"
         >
-          {{ folder.name }} <span v-if="folder.active" class="folders__list__item__active"></span>
+          {{ folder.name }}
+          <span v-if="folder.active" class="folders__list__item__active"></span>
         </div>
         <button class="mt20" @click="addFolder">Add folder</button>
       </div>
       <div class="folders__detail">
         <div v-for="(folder, index) in data.folders">
-          <Folder v-if="index === activeFolder" :folder="folder" @deleteFolder="deleteFolder" />
+          <Folder
+            v-if="index === activeFolder"
+            :folder="folder"
+            @deleteFolder="deleteFolder"
+          />
         </div>
+
+        <button class="mt20" @click="save">Save</button>
+
+        <!-- <textarea name="" id="" cols="30" rows="10" :value="JSON.stringify(data)"></textarea> -->
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { ref } from "vue"
-  import Folder from "./components/Folder.vue";
-    
-  const data = ref({
-    folders: [{
-      name: "default folder",
-      active: true,
-      tabs: [{
-        name: "cdt",
-        active: true,
-        requestHeaders: [{
-          active: true,
-          name: "test-header",
-          value: "some value",
-        }]
-      }, {
-        name: "preprod",
-        active: true,
-        requestHeaders: []
-      }]
-    }, {
-      name: "another folder",
-      active: false,
-      tabs: [{
-        name: "cdt",
-        active: true,
-        requestHeaders: [{
-          active: true,
-          name: "test-header",
-          value: "some value",
-        }]
-      }]
-    }]
-  })
+import { ref, onMounted } from "vue";
+import type { Ref } from "vue";
 
-  const activeFolder = ref(0);
+import Folder from "./components/Folder.vue";
 
-  function addFolder() {
-    data.value.folders.push({
-      name: "New folder",
+interface Tab {
+  name: string;
+  active: boolean;
+  requestHeaders?: any[];
+  responseHeaders?: any[];
+  blockedRequests?: any[];
+}
+
+interface Folder {
+  name: string;
+  active: boolean;
+  tabs: Tab[];
+}
+
+interface Data {
+  active: boolean;
+  folders: Folder[];
+}
+
+/**
+ * static variables
+ */
+
+const isChrome: boolean =
+  chrome && chrome.declarativeNetRequest && chrome.storage;
+const fallbackData = {
+  active: true,
+  folders: [
+    {
+      name: "Default folder",
       active: true,
-      tabs: [{
+      tabs: [
+        {
+          name: "Default tab",
+          active: true,
+          requestHeaders: [],
+          responseHeaders: [],
+          blockedRequests: [],
+        },
+      ],
+    },
+  ],
+};
+
+/**
+ * reactive variables
+ */
+
+const data: Ref<Data> = ref({
+  active: true,
+  folders: [],
+});
+
+const activeFolder: Ref<number> = ref(0);
+
+/**
+ * methods
+ */
+
+function addFolder() {
+  data.value.folders?.push({
+    name: "New folder",
+    active: true,
+    tabs: [
+      {
         name: "default tab",
         active: true,
-        requestHeaders: []
-      }]
-    })
+        requestHeaders: [],
+        responseHeaders: [],
+        blockedRequests: [],
+      },
+    ],
+  });
+}
+
+function deleteFolder(targetFolder: Folder) {
+  data.value.folders = data.value.folders.filter(
+    (folder) => folder !== targetFolder
+  );
+
+  activeFolder.value = 0;
+}
+
+function generateRules() {
+  const rules: any = [];
+  let id = 1;
+
+  if (!data.value.active) {
+    return [];
   }
 
-  function deleteFolder(targetFolder:any) {
-    data.value.folders = data.value.folders.filter(folder => folder !== targetFolder);
-  }
+  data.value.folders.forEach((folder) => {
+    if (folder.active) {
+      folder.tabs.forEach((tab) => {
+        if (tab.active) {
+          tab.blockedRequests?.forEach((request) => {
+            if (request.active) {
+              rules.push({
+                id,
+                priority: 1,
+                action: { type: "block" },
+                condition: request.condition,
+              });
 
+              id += 1;
+            }
+          });
+        }
+      });
+    }
+  });
+
+  return rules;
+}
+
+async function save() {
+  const newRules = generateRules();
+
+  // console.log(newRules);
+  console.log(data.value);
+
+  if (isChrome) {
+    const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
+    const oldRuleIds = oldRules.map((rule: any) => rule.id);
+
+    await chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: oldRuleIds,
+      addRules: newRules,
+    });
+
+    chrome.storage.local.set({ data: JSON.stringify(data.value) }).then(() => {
+      console.log("Chrome data is set");
+    });
+  } else {
+    window.localStorage.setItem("[ModBox]Data", JSON.stringify(data.value));
+  }
+}
+
+async function getData() {
+  const chromeData = await chrome.storage.local.get(["data"]);
+
+  //   chrome.storage.local.get(["key"]).then((result) => {
+  //   console.log("Value is " + result.key);
+  // });
+
+  console.log(chromeData.data);
+  console.log(activeFolder.value);
+
+  if (chromeData.data) {
+    console.log("restore data from chrome storage");
+    data.value = JSON.parse(chromeData.data);
+  } else {
+    console.log("nothing to restore from chrome");
+    data.value = fallbackData;
+  }
+}
+
+/**
+ * lifecycle hooks
+ */
+
+onMounted(() => {
+  if (isChrome) {
+    getData();
+  } else {
+    const lsData = window.localStorage.getItem("[ModBox]Data");
+
+    if (lsData) {
+      console.log("restore data from ls");
+      data.value = JSON.parse(lsData);
+    } else {
+      console.log("nothing to restore from ls");
+      data.value = fallbackData;
+    }
+  }
+});
 </script>
-
--- src/components/Folder.vue
+++ src/components/Folder.vue
@@ -1,14 +1,21 @@
 
 <template>
   <div class="folder">
-    <!-- <h2>{{ folder.name }}</h2> -->
-    <div class="mb20">
-      <input type="text" v-model="folder.name" placeholder="Folder name">
+    <div class="folder__header mb20">
+      <h2>{{ folder.name }}</h2>
+      <button @click="showSettings = !showSettings">Settings</button>
     </div>
-    <div class="mb20">
-      <button @click="folder.active = !folder.active">{{ folder.active ? "Disable" : "Activate"}}</button>
-      <button @click="deleteFolder()">Delete</button>
+    
+    <div v-if="showSettings" class="panel mb20">
+      <div class="mb20">
+        <input type="text" v-model="folder.name" placeholder="Folder name">
+      </div>
+      <div>
+        <button @click="folder.active = !folder.active">{{ folder.active ? "Disable" : "Activate"}}</button>
+        <button @click="deleteFolder()">Delete</button>
+      </div>
     </div>
+
     <div class="tabs mb20">
       <button class="tabs__tab" v-for="(tab, index) in folder.tabs"
         :class="{'tabs__tab--active': index === activeTab}"
@@ -23,24 +30,80 @@
       <div v-if="index === activeTab">
         <h3>{{tab.name}}</h3>
         <div class="mb20">
+          <input type="text" v-model="tab.name" placeholder="Tab name">
+        </div>
+        <div class="mb20">
           isActive: {{ tab.active}} <button @click="tab.active = !tab.active">toggle</button>
         </div>
 
-        <h2>Request headers ({{tab.requestHeaders.length}})</h2>
-        <div v-if="!tab.requestHeaders.length">No request headers</div>
-
-        <div class="request-header" v-for="(request, index) in tab.requestHeaders">
-          <div v-if="request.rule">
-            {{ request.rule.field }}
-            {{ request.rule.comparison }}
-            {{ request.rule.value }}
+        <div class="panel">
+          <h2 class="mb20">Block requests ({{tab.blockedRequests.length}})</h2>
+          <div v-if="!tab.blockedRequests.length">No blocked requests</div>
+  
+          
+          <div>
+            <div class="row" v-for="(request) in tab.blockedRequests">
+              <div class="row__col row__col--checkbox">
+                <div class="field">
+                  <input class="" type="checkbox" v-model="request.active"  />
+                </div>
+              </div>
+              <div class="row__col">
+                <div class="field">
+                  <input class="field__input" type="text" v-model="request.condition.urlFilter" placeholder="urlFilter" />
+                </div>
+              </div>
+              <!-- <div class="row__col">
+                <div class="field">
+                  <input class="field__input" type="text" v-model="request.value"  placeholder="request value" />
+                </div>
+              </div> -->
+              <div class="row__col row__col--checkbox">
+                <button @click="deleteBlockRequest(request)">Delete</button>
+              </div>
+            </div>
+  
           </div>
-
-          <input class="request-header__input" type="text" v-model="request.name" />
-          <input class="request-header__input" type="text" v-model="request.value" />
+  
+          <button class="mt20" @click="addBlockedRequest(tab)">Add request</button>
         </div>
 
-        <button class="mt20" @click="addRequestHeader(tab)">Add request header</button>
+        <div class="panel">
+          <h2 class="mb20">Request headers ({{tab.requestHeaders.length}})</h2>
+          <div v-if="!tab.requestHeaders.length">No request headers</div>
+  
+          
+          <div class="request-header">
+            <!-- <div v-if="request.rule">
+              {{ request.rule.field }}
+              {{ request.rule.comparison }}
+              {{ request.rule.value }}
+            </div> -->
+            <div class="row" v-for="(header) in tab.requestHeaders">
+              <div class="row__col row__col--checkbox">
+                <div class="field">
+                  <input class="" type="checkbox" v-model="header.active"  />
+                </div>
+              </div>
+              <div class="row__col">
+                <div class="field">
+                  <input class="field__input" type="text" v-model="header.name" placeholder="Header name" />
+                </div>
+              </div>
+              <div class="row__col">
+                <div class="field">
+                  <input class="field__input" type="text" v-model="header.value"  placeholder="Header value" />
+                </div>
+              </div>
+              <div class="row__col row__col--checkbox">
+                <button @click="deleteRequestHeader(header)">Delete</button>
+              </div>
+            </div>
+  
+          </div>
+  
+          <button class="mt20" @click="addRequestHeader(tab)">Add request header</button>
+        </div>
       </div>
     </div>
 
@@ -55,17 +118,37 @@ const emit = defineEmits(["deleteFolder"])
 
 
 const folder = props.folder;
+
 const activeTab = ref(0);
+const showSettings = ref(false);
 
 function addRequestHeader(tab:any) {
   tab.requestHeaders.push({
+    active: false,
     name: "",
-    value: ""
+    value: "",
+  })
+} 
+
+function addBlockedRequest(tab:any) {
+  tab.blockedRequests.push({
+    active: false,
+    condition: {
+      urlFilter: ""
+    }
   })
 } 
 
 function deleteFolder() {
   emit("deleteFolder", props.folder)
 } 
+
+function deleteRequestHeader(targetHeader:any) {
+  folder.tabs[activeTab.value].requestHeaders = folder.tabs[activeTab.value].requestHeaders.filter((header:any) => header !== targetHeader)
+}
+
+function deleteBlockRequest(targetRequest:any) {
+  folder.tabs[activeTab.value].blockedRequests = folder.tabs[activeTab.value].blockedRequests.filter((request:any) => request !== targetRequest)
+}
 
 </script>
